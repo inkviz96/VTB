f085b71ea5015496f4d2553cf20b4c6aca49d30a
dgolov <Dima5671356713567>
2021-10-09 18:53:22 +0300

    create join_dataset and login functions

--- routing/datasets.py
+++ routing/datasets.py
@@ -16,27 +16,14 @@ async def dataset_list():
     """
     Получение всех datasets
     """
-    session = requests.Session()
-    data = {"username": "datahub", "password": "datahub"}
-    session.post("http://datahub.yc.pbd.ai:9002/logIn", json=data)
-    result = {}
-    datasets = {
-        'hive': ('SampleHiveDataset', 'fct_users_created', 'fct_users_deleted', 'logging_events'),
-        'hdfs': ('SampleHdfsDataset', ),
-        'kafka': ('SampleKafkaDataset',)
-    }
-    for dataset_type, dataset_name in datasets.items():
-        type_list = {dataset_type: []}
-        for name in dataset_name:
-            response = get_dataset(session, dataset_type, name)
-            type_list[dataset_type].append(response)
-        result[dataset_type] = (type_list[dataset_type])
-    return JSONResponse(content=result, status_code=status.HTTP_200_OK)
+    connect = await login()
+    generated_dataset = await join_dataset(connect)
+    return JSONResponse(content=generated_dataset, status_code=status.HTTP_200_OK)
 
 
-def get_dataset(session, dataset_type: str, dataset_name: str):
-    """ В первую строчку запроса вместо hive и SampleHiveDataset (как было ранее)
-        подставляем тип и название dataset (из url) и получаем его данные
+def get_dataset(connect, dataset_type: str, dataset_name: str):
+    """
+    Получения одного датасет
     hive dataset list:
         SampleHiveDataset
         fct_users_created
@@ -46,7 +33,6 @@ def get_dataset(session, dataset_type: str, dataset_name: str):
         SampleHdfsDataset
     kafka dataset list:
         SampleKafkaDataset
-    Example: http://0.0.0.0:8017/api/v1/dataset_list/hife/fct_users_created
     """
     data = {"query": """{
         dataset(urn: "urn:li:dataset:(urn:li:dataPlatform:%s,%s,PROD)"){
@@ -81,7 +67,7 @@ def get_dataset(session, dataset_type: str, dataset_name: str):
             }
         }
     }""" % (dataset_type, dataset_name)}
-    response = session.post("http://datahub.yc.pbd.ai:9002/api/graphql", json=data)
+    response = connect.post("http://datahub.yc.pbd.ai:9002/api/graphql", json=data)
     return json.loads(response.text)
 
 
@@ -120,24 +106,11 @@ async def users_dataset_list():
 
 @router.post("/new_dataset/", tags=["datasets"], status_code=200)
 async def new_dataset(user_id: int, data_name: str, data_sell: bool, data_price: str):
-    new_dataset = {}
-    connect = requests.Session()
-    data = {"username": "datahub", "password": "datahub"}
-    connect.post("http://datahub.yc.pbd.ai:9002/logIn", json=data)
-    datasets = {
-        'hive': ('SampleHiveDataset', 'fct_users_created', 'fct_users_deleted', 'logging_events'),
-        'hdfs': ('SampleHdfsDataset', ),
-        'kafka': ('SampleKafkaDataset',)
-    }
-    for dataset_type, dataset_name in datasets.items():
-        type_list = {dataset_type: []}
-        for name in dataset_name:
-            response = get_dataset(connect, dataset_type, name)
-            type_list[dataset_type].append(response)
-        new_dataset[dataset_type] = (type_list[dataset_type])
+    connect = await login()
+    generated_dataset = await join_dataset(connect)
     dataset = session.add(Dataset(name=data_name,
                                   status='pending',
-                                  data=new_dataset,
+                                  data=generated_dataset,
                                   sell=data_sell,
                                   price=data_price))
     user = session.query(User).filter_by(id=user_id)
@@ -145,3 +118,32 @@ async def new_dataset(user_id: int, data_name: str, data_sell: bool, data_price:
     session.commit()
 
     return JSONResponse(status_code=status.HTTP_200_OK)
+
+
+async def login():
+    """
+    LogIn
+    """
+    connect = requests.Session()
+    data = {"username": "datahub", "password": "datahub"}
+    connect.post("http://datahub.yc.pbd.ai:9002/logIn", json=data)
+    return connect
+
+
+async def join_dataset(connect):
+    """
+    Объединение запрашиваемых dataset
+    """
+    result = {}
+    datasets = {
+        'hive': ('SampleHiveDataset', 'fct_users_created', 'fct_users_deleted', 'logging_events'),
+        'hdfs': ('SampleHdfsDataset',),
+        'kafka': ('SampleKafkaDataset',)
+    }
+    for dataset_type, dataset_name in datasets.items():
+        type_list = {dataset_type: []}
+        for name in dataset_name:
+            response = get_dataset(connect, dataset_type, name)
+            type_list[dataset_type].append(response)
+        result[dataset_type] = (type_list[dataset_type])
+    return result
