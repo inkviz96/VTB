ed01410c4e306e476b275bc744b356dc949f70cd
dgolov <Dima5671356713567>
2021-10-09 10:24:13 +0300

    expanding query parameters

--- routing/datasets.py
+++ routing/datasets.py
@@ -1,4 +1,3 @@
-import requests
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 import requests
@@ -6,11 +5,56 @@ import requests
 router = APIRouter(prefix="/api/v1")
 
 
-@router.get("/dataset_list/", status_code=200)
-async def dataset_list():
+@router.get("/dataset_list/{dataset_type}/{dataset_name}", status_code=200)
+async def dataset_list(dataset_type: str, dataset_name: str):
+    """ В первую строчку запроса вместо hive и SampleHiveDataset (как было ранее)
+        подставляем тип и название dataset (из url) и получаем его данные
+    hive dataset list:
+        SampleHiveDataset
+        fct_users_created
+        fct_users_deleted
+        logging_events
+    hdfs dataset list:
+        SampleHdfsDataset
+    kafka dataset list:
+        SampleKafkaDataset
+    Example: http://0.0.0.0:8017/api/v1/dataset_list/fife/fct_users_created
+    """
     session = requests.Session()
     data = {"username": "datahub", "password": "datahub"}
     session.post("http://datahub.yc.pbd.ai:9002/logIn", json=data)
-    data = {"query": '{dataset(urn: "urn:li:dataset:(urn:li:dataPlatform:hive,SampleHiveDataset,PROD)"){name}}'}
+    data = {"query": """{
+        dataset(urn: "urn:li:dataset:(urn:li:dataPlatform:%s,%s,PROD)"){
+            urn
+            name
+            tags {
+                tags {
+                    tag {
+                        name
+                    }
+                }
+            }
+            schemaMetadata(version: 0) {
+                version
+                name
+                fields {
+                    fieldPath
+                    description
+                    type
+                    tags {
+                        tags {
+                            tag {
+                                name
+                            }
+                        }
+                    }
+                }
+                primaryKeys
+                foreignKeys {
+                    name
+                }
+            }
+        }
+    }""" % (dataset_type, dataset_name)}
     response = session.post("http://datahub.yc.pbd.ai:9002/api/graphql", json=data)
     return JSONResponse(content=response.text, status_code=status.HTTP_200_OK)
